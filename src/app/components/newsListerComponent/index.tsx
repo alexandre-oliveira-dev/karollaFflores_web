9412c3525dc78baf360e79c411769fd277e556d0
alexandre-oliveira-dev <alexandredev.frontend@gmail.com>
2024-08-05 22:16:37 -0300

    checkpoint

---
+++ src/app/components/newsListerComponent/index.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+import "./style.css";
+import Title from "antd/es/typography/Title";
+
+export default function NewsListerComponent() {
+  return (
+    <section className="container-newslister">
+      <Title level={1}>Novidades</Title>
+    </section>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,12 @@ import "./page.css";
 import MainComponent from "./components/mainComponent";
 import logo from "./assets/logo-white.png";
 import logo2 from "./assets/logo2.png";
+import {Row} from "antd";
+import deliveryIcon from "./assets/icons/Delivery.png";
+import discontIcon from "./assets/icons/Discount.png";
+import whatsIcon from "./assets/icons/WhatsApp.png";
+import Title from "antd/es/typography/Title";
+import NewsListerComponent from "./components/newsListerComponent";
 
 export default function Home() {
   return (
@@ -31,6 +37,55 @@ export default function Home() {
               </h1>
             </div>
           </div>
+          <Row
+            style={{
+              justifyContent: "space-evenly",
+              alignItems: "center",
+              height: "83px",
+              backgroundColor: "#fff",
+            }}
+          >
+            <Row style={{alignItems: "center", gap: 20}}>
+              <div>
+                <img
+                  height={50}
+                  width={50}
+                  src={deliveryIcon?.src}
+                  alt="deliveryicon"
+                />
+              </div>
+              <Title style={{color: "#FF5353"}} level={3}>
+                Entrega rápida
+              </Title>
+            </Row>
+            <Row style={{alignItems: "center", gap: 20}}>
+              <div>
+                <img
+                  height={50}
+                  width={50}
+                  src={whatsIcon?.src}
+                  alt="deliveryicon"
+                />
+              </div>
+              <Title style={{color: "#FF5353"}} level={3}>
+                Compre pelo whatsapp
+              </Title>
+            </Row>
+            <Row style={{alignItems: "center", gap: 20}}>
+              <div>
+                <img
+                  height={50}
+                  width={50}
+                  src={deliveryIcon?.src}
+                  alt="deliveryicon"
+                />
+              </div>
+              <Title style={{color: "#FF5353"}} level={3}>
+                Descontos de até 10%
+              </Title>
+            </Row>
+          </Row>
+          <NewsListerComponent></NewsListerComponent>
         </main>
       </MainComponent>
     </>
